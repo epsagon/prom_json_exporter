05b1705eff471d323c2006f10a845e5989a7a0ee
Jan Schulte <jan@epsagon.com>
2021-09-03 10:03:29 -0400

    Fetch json from endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::{AppSettings, Clap};
 
-
 /// This doc string acts as a help message when the user runs '--help'
 /// as do all doc strings on fields
 #[derive(Clap)]
@@ -10,10 +9,28 @@ struct Opts {
     json_endpoint: String
 }
 
-fn main() {
+async fn fetch_json(json_endpoint: String) -> Result<String, Box<dyn std::error::Error>> {
+    let res = reqwest::get(json_endpoint).await?;
+    let body = res.text().await?;
+    Ok(body)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Prom JSON Exporter");
     let opts: Opts = Opts::parse();
 
     println!("reading {}", opts.json_endpoint);
+
+    match fetch_json(opts.json_endpoint).await {
+        Ok(body) => {
+            println!("{}", body);
+        },
+        Err(err) => {
+            println!("{:?}", err);
+        }
+    }
+
+    Ok(())
 }
 
